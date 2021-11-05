e0a7ee0e36bf38e496282c9e94d3ce97d592c729
DAW <ifc303daw@gmail.com>
2021-11-05 12:56:44 +0100

    correction

--- resources/insert_usuarios.sql
+++ resources/insert_usuarios.sql
@@ -1,10 +1,10 @@
-/*
--- Query: SELECT * FROM disc.usuarios
-LIMIT 0, 1000
+-- SET FOREIGN_KEY_CHECKS = 0;
+-- TRUNCATE TABLE `disc`.`usuarios`;
+-- TRUNCATE TABLE `disc`.`centros`;
+-- SET FOREIGN_KEY_CHECKS = 1;
 
--- Date: 2021-11-05 12:06
-*/
-INSERT INTO `` (`dni`,`nombre`,`apellidos`,`clave`,`centros_nombre`,`tipo`) VALUES ('1111','admin general','admin general','1111','CPIFP Bajo Aragon','admin general');
-INSERT INTO `` (`dni`,`nombre`,`apellidos`,`clave`,`centros_nombre`,`tipo`) VALUES ('2222','admin centro','admin centro','2222','CPIFP Bajo Aragon','admin centro');
-INSERT INTO `` (`dni`,`nombre`,`apellidos`,`clave`,`centros_nombre`,`tipo`) VALUES ('3333','profe','profe','3333','CPIFP Bajo Aragon','profesor');
-INSERT INTO `` (`dni`,`nombre`,`apellidos`,`clave`,`centros_nombre`,`tipo`) VALUES ('4444','alumno','alumno','4444','CPIFP Bajo Aragon','alumno');
+INSERT INTO `disc`.`centros` (`nombre`, `localidad`, `codigopostal`) VALUES ('Q4400415H', 'Alcañíz', '44600');
+INSERT INTO `disc`.`usuarios` (`dni`, `nombre`, `apellidos`, `clave`, `centros_nombre`, `tipo`) VALUES ('1111', 'admin general', 'admin general', '1111', 'Q4400415H', 'admin general');
+INSERT INTO `disc`.`usuarios` (`dni`,`nombre`,`apellidos`,`clave`,`centros_nombre`,`tipo`) VALUES ('2222','admin centro','admin centro','2222','CPIFP Bajo Aragon','admin centro');
+INSERT INTO `disc`.`usuarios` (`dni`,`nombre`,`apellidos`,`clave`,`centros_nombre`,`tipo`) VALUES ('3333','profe','profe','3333','CPIFP Bajo Aragon','profesor');
+INSERT INTO `disc`.`usuarios` (`dni`,`nombre`,`apellidos`,`clave`,`centros_nombre`,`tipo`) VALUES ('4444','alumno','alumno','4444','CPIFP Bajo Aragon','alumno');
