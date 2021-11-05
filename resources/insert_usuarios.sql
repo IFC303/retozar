e6da817104abf9a8a1eea42f54f7311faf9101b7
sivlia <sielma712@gmail.com>
2021-11-05 12:08:17 +0100

    subo el insert usuarios

---
+++ resources/insert_usuarios.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM disc.usuarios
+LIMIT 0, 1000
+
+-- Date: 2021-11-05 12:06
+*/
+INSERT INTO `` (`dni`,`nombre`,`apellidos`,`clave`,`centros_nombre`,`tipo`) VALUES ('1111','admin general','admin general','1111','CPIFP Bajo Aragon','admin general');
+INSERT INTO `` (`dni`,`nombre`,`apellidos`,`clave`,`centros_nombre`,`tipo`) VALUES ('2222','admin centro','admin centro','2222','CPIFP Bajo Aragon','admin centro');
+INSERT INTO `` (`dni`,`nombre`,`apellidos`,`clave`,`centros_nombre`,`tipo`) VALUES ('3333','profe','profe','3333','CPIFP Bajo Aragon','profesor');
+INSERT INTO `` (`dni`,`nombre`,`apellidos`,`clave`,`centros_nombre`,`tipo`) VALUES ('4444','alumno','alumno','4444','CPIFP Bajo Aragon','alumno');
