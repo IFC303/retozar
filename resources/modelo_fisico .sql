4a1c8663c41a3b98dd7b2288dd8710e0eb1fff7c
manauta <raul.dkl.37@gmail.com>
2021-11-05 12:06:21 +0100

    Modificación tabla centros

--- resources/modelo_fisico .sql
+++ resources/modelo_fisico .sql
@@ -1,12 +1,17 @@
 
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
 CREATE SCHEMA IF NOT EXISTS `disc` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `disc` ;
-
 -- -----------------------------------------------------
 -- Table `disc`.`centros`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `disc`.`centros` (
-  `nombre` varchar(45) NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  `localidad` VARCHAR(100) NOT NULL,
+  `codigopostal` VARCHAR(5) NOT NULL,
   PRIMARY KEY (`nombre`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
@@ -17,11 +22,11 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `disc`.`usuarios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `disc`.`usuarios` (
-  `dni` varchar(9) NOT NULL,
+  `dni` VARCHAR(9) NOT NULL,
   `nombre` VARCHAR(45) NULL DEFAULT NULL,
   `apellidos` VARCHAR(100) NULL DEFAULT NULL,
   `clave` VARCHAR(45) NULL DEFAULT NULL,
-  `centros_nombre` varchar(45) NOT NULL,
+  `centros_nombre` VARCHAR(45) NOT NULL,
   `tipo` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`dni`),
   INDEX `fk_usuarios_centros_idx` (`centros_nombre` ASC) VISIBLE,
@@ -37,7 +42,7 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `disc`.`admins`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `disc`.`admins` (
-  `usuarios_dni` varchar(9) NOT NULL,
+  `usuarios_dni` VARCHAR(9) NOT NULL,
   PRIMARY KEY (`usuarios_dni`),
   INDEX `fk_admins_usuarios1_idx` (`usuarios_dni` ASC) VISIBLE,
   CONSTRAINT `fk_admins_usuarios1`
@@ -54,7 +59,7 @@ COLLATE = utf8mb4_0900_ai_ci;
 CREATE TABLE IF NOT EXISTS `disc`.`cursos` (
   `codigo` INT NOT NULL,
   `departamento` VARCHAR(45) NULL DEFAULT NULL,
-  `centros_nombre` varchar(45) NOT NULL,
+  `centros_nombre` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`codigo`, `centros_nombre`),
   INDEX `fk_cursos_centros1_idx` (`centros_nombre` ASC) VISIBLE,
   CONSTRAINT `fk_cursos_centros1`
@@ -80,10 +85,10 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `disc`.`alumnos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `disc`.`alumnos` (
-  `usuarios_dni` varchar(9) NOT NULL,
+  `usuarios_dni` VARCHAR(9) NOT NULL,
   `equipos_nombre` VARCHAR(45) NOT NULL,
   `cursos_codigo` INT NOT NULL,
-  `cursos_centros_nombre` varchar(45) NOT NULL,
+  `cursos_centros_nombre` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`usuarios_dni`),
   INDEX `fk_alumnos_equipos1_idx` (`equipos_nombre` ASC) VISIBLE,
   INDEX `fk_alumnos_cursos1_idx` (`cursos_codigo` ASC, `cursos_centros_nombre` ASC) VISIBLE,
@@ -119,7 +124,7 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `disc`.`alumnos_has_preguntas`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `disc`.`alumnos_has_preguntas` (
-  `alumnos_usuarios_dni` varchar(9) NOT NULL,
+  `alumnos_usuarios_dni` VARCHAR(9) NOT NULL,
   `preguntas_numero` INT NOT NULL,
   `respuestas` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`alumnos_usuarios_dni`, `preguntas_numero`),
@@ -140,7 +145,7 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `disc`.`profesores`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `disc`.`profesores` (
-  `usuarios_dni` varchar(9) NOT NULL,
+  `usuarios_dni` VARCHAR(9) NOT NULL,
   `departamento` VARCHAR(45) NULL DEFAULT NULL,
   `isAdmin` TINYINT NULL DEFAULT NULL,
   PRIMARY KEY (`usuarios_dni`),
@@ -157,24 +162,23 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `disc`.`profesores_has_cursos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `disc`.`profesores_has_cursos` (
-  `profesores_usuarios_dni` varchar(9) NOT NULL,
+  `profesores_usuarios_dni` VARCHAR(9) NOT NULL,
   `cursos_codigo` INT NOT NULL,
-  `cursos_centros_nombre` varchar(45) NOT NULL,
+  `cursos_centros_nombre` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`profesores_usuarios_dni`, `cursos_codigo`, `cursos_centros_nombre`),
   INDEX `fk_profesores_has_cursos_cursos1_idx` (`cursos_codigo` ASC, `cursos_centros_nombre` ASC) VISIBLE,
   INDEX `fk_profesores_has_cursos_profesores1_idx` (`profesores_usuarios_dni` ASC) VISIBLE,
-  CONSTRAINT `fk_profesores_has_cursos_profesores1`
-    FOREIGN KEY (`profesores_usuarios_dni`)
-    REFERENCES `disc`.`profesores` (`usuarios_dni`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_profesores_has_cursos_cursos1`
     FOREIGN KEY (`cursos_codigo` , `cursos_centros_nombre`)
-    REFERENCES `disc`.`cursos` (`codigo` , `centros_nombre`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES `disc`.`cursos` (`codigo` , `centros_nombre`),
+  CONSTRAINT `fk_profesores_has_cursos_profesores1`
+    FOREIGN KEY (`profesores_usuarios_dni`)
+    REFERENCES `disc`.`profesores` (`usuarios_dni`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
