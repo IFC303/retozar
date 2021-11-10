01e53fe4cdb3a370edc5381b328e903440e97c08
IFC303 <72009474+IFC303@users.noreply.github.com>
2021-11-10 14:37:56 +0100

    Update modelo_datos.sql

--- resources/modelo_datos.sql
+++ resources/modelo_datos.sql
@@ -1,7 +1,13 @@
 SET FOREIGN_KEY_CHECKS=0;
-TRUNCATE TABLE `disc`.`centros`;
-TRUNCATE TABLE `disc`.`usuarios`;
 TRUNCATE TABLE `disc`.`admins`;
+TRUNCATE TABLE `disc`.`alumnos`;
+TRUNCATE TABLE `disc`.`alumnos_has_preguntas`;
+TRUNCATE TABLE `disc`.`centros`;
+TRUNCATE TABLE `disc`.`equipos`;
+TRUNCATE TABLE `disc`.`preguntas`;
+TRUNCATE TABLE `disc`.`profesores`;
+TRUNCATE TABLE `disc`.`profesores_has_cursos`;
+TRUNCATE TABLE `disc`.`usuarios`;
 SET FOREIGN_KEY_CHECKS=1;
 
 INSERT INTO `disc`.`centros` (`nombre`, `localidad`, `codigopostal`) VALUES ('Q4400415H', 'Alcañíz', '44600');
@@ -12,3 +18,5 @@ INSERT INTO `disc`.`usuarios` (`dni`, `nombre`, `apellidos`, `clave`, `centros_n
 INSERT INTO `disc`.`usuarios` (`dni`, `nombre`, `apellidos`, `clave`, `centros_nombre`, `tipo`) VALUES ('4444', 'alumno','alumno','4444', 'Q4400415H', 'alumno');
 
 INSERT INTO `disc`.`admins` (`usuarios_dni`) VALUES ('1111');
+INSERT INTO `disc`.`profesores` (`usuarios_dni`, `departamento`, `isAdmin`) VALUES ('3333', 'informatica', true);
+-- INSERT INTO `disc`.`alumnos` (`usuarios_dni`, `equipos_nombre`, `cursos_codigo`, `cursos_centros_nombre`) VALUES ('4444', '0', '0', 'Q4400415H');
