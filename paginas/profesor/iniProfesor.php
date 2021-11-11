7adcbf7abe39e738d22c6a878b3a607511dccdc1
manauta <raul.dkl.37@gmail.com>
2021-11-11 12:40:38 +0100

    iniprofesor + css

--- paginas/profesor/iniProfesor.php
+++ paginas/profesor/iniProfesor.php
@@ -2,59 +2,87 @@
     session_start();
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+        include "./../../includes/funciones.php";
+    ?>   
+    <link rel="stylesheet" href="../../css/inipro.css"> 
+</head>
+    <body>
+        <main>
+        <div id="texto" >
+            <p id="t1">Disc Aragón</p>
+            <p id="t2">Test para formación de equipos de trabajo</p>
+        </div>
+        
 
-        <head>
-            <?php
-                include "./../../includes/funciones.php";
-            ?>    
-        </head>
+    <div id="contenedormain">    
+    <div class="bienv">
+        <?php
+            echo "Bienvenido ".$_SESSION['nombreL'];
+        ?>
+    </div>
 
+    <div id="contenedoraux">
 
-        <body>
-
-                <h2>Area de profesores</h2>
-            
-                <?php
-                    echo "Bienvenido ".$_SESSION['nombreL'];
-                ?>
-
-
-                <br><br>
-                
-
-                    
-                            <a name="alumnos" onclick="mostrar(this.name);">Gestion alumnos</a>
-                            <br><br>
-                            <a name="grupos" onclick="mostrar(this.name);">Gestion grupos</a>
-                            <br><br>
-                            <a name="resultados" onclick="mostrar(this.name);">Ver resultados</a>
-                   
-                
-
-                    <ul id="gAlumnos" style="display:none">
-                        <li><a href="altaAlumnos.php">Alta alumnos</a></li>
-                        <li><a href="verAlumnos.php">Ver alumnos</a></li>
-                    </ul>              
+    <div id="enlaces">
+        <ul>
+            <li><a name="alumnos" onclick="mostrar(this.name);">Gestion alumnos</a></li>
+            <li><a name="grupos" onclick="mostrar(this.name);">Gestion grupos</a></li>
+            <li><a name="resultados" onclick="mostrar(this.name);">Ver resultados</a></li>
+        </ul>
+    </div> <!-- fin enlaces -->     
+          
+    <div id="result">
+    
+        <ul id="gAlumnos" style="display:none">
+            <li><a href="altaAlumnos.php">Alta alumnos</a></li><br>
+            <li><a href="verAlumnos.php">Ver alumnos</a></li>
+        </ul>              
                
-                    <ul id="gGrupos" style="display:none">
-                        <li><a href="#">Alta grupo</a></li>
-                        <li><a href="#">Ver grupos</a></li>
-                    </ul>
+        <ul id="gGrupos" style="display:none">
+            <li><a href="#">Alta grupo</a></li><br>
+            <li><a href="#">Ver grupos</a></li>
+        </ul>
 
-                    <ul id="resultados" style="display:none">
-                        <li><a href="#">Resultado por alumno</a></li>
-                        <li><a href="#">Resultado por grupos</a></li>
-                    </ul>
+        <ul id="resultados" style="display:none">
+            <li><a href="#">Resultado por alumno</a></li><br>
+            <li><a href="#">Resultado por grupos</a></li>
+        </ul>
 
-                    <br><br>
-                
+    </div> <!-- fin result -->
+
+    </div> <!-- fin contenedoraux-->
+
+    </div> <!-- fin contenedor main -->
 
 
-        </body>
+        </main> <!-- fin main -->
+        
+        <footer>
+            <div id="img1">
+                <picture>
+                    <source media="(min-width:650px)" srcset="../../img/logocpfip.png">
+                    <source media="(min-width:465px)" srcset="../../img/logocpfip_bkzdvb_c_scale,w_200.png">
+                    <img src="../../img/logocpfip_bkzdvb_c_scale,w_200.png" alt="Flowers" style="width:auto;">
+                </picture>
+            </div> <!-- fin img1 -->
 
+            <div id="img2">
+                <img src="../../img/logoSBR.png" alt="logo_SBR" width="95px" height="95px">
+            </div> <!-- fin img2 -->
+
+        </footer><!-- fin footer -->
+
+        </body> <!-- fin body -->
+</html>
 
     <script src="./../iniMostrar.js"></script>
 
 
-</html>
