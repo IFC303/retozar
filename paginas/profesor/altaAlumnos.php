5badf231fcd1f2095295737fa219ea8c3035f1aa
sivlia <sielma712@gmail.com>
2021-11-15 11:58:06 +0100

    includes en  profesor

--- paginas/profesor/altaAlumnos.php
+++ paginas/profesor/altaAlumnos.php
@@ -1,6 +1,9 @@
 <?php
     session_start();
     include "./../../includes/funciones.php";
+    include "./../../includes/alta.php";
+    include "./../../includes/baja.php";
+    include "./../../includes/modi.php";
     
 ?>
 <!DOCTYPE html>
