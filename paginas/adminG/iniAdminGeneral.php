d2a62c7c1fdfc47226840d7b13c453c5eb9f1b9d
DAW <ifc303daw@gmail.com>
2021-11-16 09:50:17 +0100

    format

--- paginas/adminG/iniAdminGeneral.php
+++ paginas/adminG/iniAdminGeneral.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+session_start();
 ?>
 
 
@@ -11,40 +11,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php
-        include "./../../includes/funciones.php";
-    ?>  
+    include "./../../includes/funciones.php";
+    ?>
 </head>
 <body>
 
-            <h2>Area Admin general</h2>
-            
-            <?php
-                echo "Bienvenido ".$_SESSION['nombreL'];
-            ?>
+<h2>Area Admin general</h2>
 
-      
-            <br><br>
-              <ul>
-                  <li><a name="centro" onclick="mostrar(this.name);">Gestion centros</a></li>
-                  <br>
-                  <li><a name="test" onclick="mostrar(this.name);">Gestion test</a></li>
-                  <br>
-              </ul> 
+<?php
+echo "Bienvenido " . $_SESSION['nombreL'];
+?>
 
-        <div id="result" style="visibility:hidden">
-              
-            <ul id="gCentro" style="display:none">
-                <li><a href="altaCentro.php">Alta centro</a></li><br>
-                <li><a href="verCentro.php">Ver ventros</a></li>
-            </ul>     
-             
-            <ul id="gTest" style="display:none">
-                <li><a href="#">Nuevo test</a></li><br>
-            </ul>
-        
-        </div>
 
-    
+<br><br>
+<ul>
+    <li><a name="centro" onclick="mostrar(this.name);">Gestion centros</a></li>
+    <br>
+    <li><a name="test" onclick="mostrar(this.name);">Gestion test</a></li>
+    <br>
+</ul>
+
+<div id="result" style="visibility:hidden">
+
+    <ul id="gCentro" style="display:none">
+        <li><a href="altaCentro.php">Alta centro</a></li>
+        <br>
+        <li><a href="verCentro.php">Ver ventros</a></li>
+    </ul>
+
+    <ul id="gTest" style="display:none">
+        <li><a href="#">Nuevo test</a></li>
+        <br>
+    </ul>
+
+</div>
+
+
 </body>
 </html>
 
