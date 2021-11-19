ac938b5e191a2bbc97d6e5d92a2aa19da0691001
IFC303 <72009474+IFC303@users.noreply.github.com>
2021-11-19 11:35:45 +0100

    preguntas -> clean up

--- paginas/adminG/iniAdminGeneral.php
+++ paginas/adminG/iniAdminGeneral.php
@@ -1,79 +1,80 @@
 <?php
-    session_start();
-    include "./../../includes/funciones.php";
+session_start();
+include "./../../includes/funciones.php";
 ?>
 
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>   
-    <link rel="stylesheet" href="../../css/inipro.css"> 
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/inipro.css">
     <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,400;0,600;0,700;1,500&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <main>
 
-            <div id="texto" >
-                <p id="t1">Disc Aragón</p>
-                <p id="t2">Test para formación de equipos de trabajo</p>
+        <div id="texto">
+            <p id="t1">Disc Aragón</p>
+            <p id="t2">Test para formación de equipos de trabajo</p>
+        </div>
+
+        <div id="contenedormain">
+
+            <div class="bienv">
+                <?php
+                nombreLog();
+                ?>
             </div>
+            <a href="../cerrarSesion.php" class="btnvolver">Cerrar sesión</a>
 
-            <div id="contenedormain">    
-                    
-                    <div class="bienv">
-                        <?php
-                            nombreLog();
-                        ?>
-                    </div>
-                    <a href="../cerrarSesion.php" class="btnvolver">Cerrar sesión</a>
+            <div id="contenedoraux">
 
-                    <div id="contenedoraux">
+                <div id="enlaces">
+                    <ul>
+                        <li><a name="centro" onclick="mostrar(this.name);">Gestion centros</a></li>
+                        <li><a name="test" onclick="mostrar(this.name);">Gestion test</a></li>
+                    </ul>
+                </div> <!-- fin enlaces -->
 
-                        <div id="enlaces">
-                            <ul>
-                                <li><a name="centro" onclick="mostrar(this.name);">Gestion centros</a></li>
-                                <li><a name="test" onclick="mostrar(this.name);">Gestion test</a></li>
-                            </ul>
-                        </div> <!-- fin enlaces -->   
+                <div id="result" style="visibility:hidden">
+                    <ul id="gCentro" style="display:none">
+                        <li><a href="altaCentro.php">Alta centro</a></li><br>
+                        <li><a href="verCentro.php">Ver centros</a></li>
+                    </ul>
+                    <ul id="gTest" style="display:none">
+                        <li><a href="#">Nuevo test</a></li><br>
+                    </ul>
+                </div> <!-- fin result -->
 
-                    <div id="result" style="visibility:hidden">
-                        <ul id="gCentro" style="display:none">
-                            <li><a href="altaCentro.php">Alta centro</a></li><br>
-                            <li><a href="verCentro.php">Ver centros</a></li>
-                        </ul>     
-                        <ul id="gTest" style="display:none">
-                            <li><a href="#">Nuevo test</a></li><br>
-                        </ul>
-                    </div> <!-- fin result -->
+            </div> <!-- fin contenedoraux-->
 
-                </div> <!-- fin contenedoraux-->
+        </div> <!-- fin contenedor main -->
 
-            </div> <!-- fin contenedor main -->
-             
-            </main> <!-- fin main -->
-        
+    </main> <!-- fin main -->
 
-        <footer>
-            <div id="img1">
-                <picture>
-                    <source media="(min-width:650px)" srcset="../../img/logocpfip.png">
-                    <source media="(min-width:465px)" srcset="../../img/logocpfip_bkzdvb_c_scale,w_200.png">
-                    <img src="../../img/logocpfip_bkzdvb_c_scale,w_200.png" alt="Flowers" style="width:auto;">
-                </picture>
-            </div> <!-- fin img1 -->
 
-            <div id="img2">
-                <img src="../../img/logoSBR.png" alt="logo_SBR" width="95px" height="95px">
-            </div> <!-- fin img2 -->
+    <footer>
+        <div id="img1">
+            <picture>
+                <source media="(min-width:650px)" srcset="../../img/logocpfip.png">
+                <source media="(min-width:465px)" srcset="../../img/logocpfip_bkzdvb_c_scale,w_200.png">
+                <img src="../../img/logocpfip_bkzdvb_c_scale,w_200.png" alt="Flowers" style="width:auto;">
+            </picture>
+        </div> <!-- fin img1 -->
 
-        </footer><!-- fin footer -->
+        <div id="img2">
+            <img src="../../img/logoSBR.png" alt="logo_SBR" width="95px" height="95px">
+        </div> <!-- fin img2 -->
 
-        </body> <!-- fin body -->
+    </footer><!-- fin footer -->
+
+</body> <!-- fin body -->
 
 </html>
 
