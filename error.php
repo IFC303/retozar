ff38025a45d0dd6d8ff04a65874c1f20841a11ea
manauta <raul.dkl.37@gmail.com>
2021-11-24 10:20:13 +0100

    pagina error.php y cambios .htaccess

--- error.php
+++ error.php
@@ -1,2 +1,38 @@
-<?php
-echo "error";
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/altaalum.css">
+</head>
+<body>
+    <header>
+        <div id="texto" >
+            <p id="t1">Disc Aragón</p>
+            <p id="t2">Test para formación de equipos de trabajo</p>
+        </div>
+    </header>
+    <main>
+        <div id="contenido">
+            <p id="t1">Página no encontrada</p>
+            <a href="/" class="btnvolver">Volver</a>
+        </div>
+    </main>
+    <footer>
+        <div id="img1">
+            <picture>
+                <source media="(min-width:650px)" srcset="../../img/logocpfip.png">
+                <source media="(min-width:465px)" srcset="../../img/logocpfip_bkzdvb_c_scale,w_200.png">
+                <img src="img/logocpfip_bkzdvb_c_scale,w_200.png" alt="Flowers" style="width:auto;">
+            </picture>
+        </div> <!-- fin img1 -->
+
+        <div id="img2">
+            <img src="img/logoSBR.png" alt="logo_SBR" width="95px" height="95px">
+        </div> <!-- fin img2 -->
+    </footer>
+</body>
+</html>
