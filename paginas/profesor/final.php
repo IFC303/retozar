3dbb10b73660e864ae080f4bbd94aa3d63b1b5b2
sivlia <sielma712@gmail.com>
2021-12-09 14:32:09 +0100

    final

--- paginas/profesor/final.php
+++ paginas/profesor/final.php
@@ -22,7 +22,7 @@
 
         <?php
                 if (isset($_POST['guardar'])){
-                echo $_POST['enviar'];
+               // echo $_POST['enviar'];
                 } else{
                 //    header('Location: url=localhost/no');
                 }
