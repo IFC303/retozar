61fe33f3cd78f9fe1b5b2a6b835d80ba706a8832
manauta <raul.dkl.37@gmail.com>
2021-12-09 12:44:11 +0100

    final.php

--- paginas/profesor/final.php
+++ paginas/profesor/final.php
@@ -1,7 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/altaalum.css"> 
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,400;0,600;0,700;1,500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+       <div id="texto" >
+            <h1 id="t1"><a href="/" class="title">Disc Aragón</a></h1>
+            <p id="t2">Test para formación de equipos de trabajo</p>
+        </div> 
+    </header>
+    <main>
 
-<?php
-if (isset($_POST['guardar'])){
-echo $_POST['enviar'];
-} else{
-//    header('Location: url=localhost/no');
-}
+    <div id="contenido">
+        <h3>Grupos insertados correctamente</h3>
+
+        <?php
+                if (isset($_POST['guardar'])){
+                echo $_POST['enviar'];
+                } else{
+                //    header('Location: url=localhost/no');
+                }
+        ?>
+         <br>
+        <a class="btnvolver" href="." > Volver al menú</a>
+    </div><!-- fin contenido -->
+    </main>
+    <footer>
+            <div id="img1">
+                <picture>
+                    <source media="(min-width:650px)" srcset="../../img/logocpfip.png">
+                    <source media="(min-width:465px)" srcset="../../img/logocpfip_bkzdvb_c_scale,w_200.png">
+                    <img src="../../img/logocpfip_bkzdvb_c_scale,w_200.png" alt="Flowers" style="width:auto;">
+                </picture>
+            </div> <!-- fin img1 -->
+
+            <div id="img2">
+                <picture>
+                    <source media="(min-width:650px)" srcset="../../img/logoSBR.png" width="95px" height="95px">
+                    <img src="../../img/logoSBR.png" alt="logo_SBR" width="70px" height="70px">
+                </picture>
+               
+            </div> <!-- fin img2 -->
+
+    </footer><!-- fin footer -->
+
+</body>
+</html>
